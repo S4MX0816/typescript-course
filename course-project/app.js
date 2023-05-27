e6894e4298a21d21503b33821e9376af7e926e89
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-05-28 02:42:04 +0530

    using enum

--- course-project/app.js
+++ course-project/app.js
@@ -2,14 +2,35 @@
 //   name: string;
 //   age: number;
 // };
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Sanyam",
+//   age: 25,
+//   hobbies: ["Music", "Movies"],
+//   role: [2, "Author"],
+// };
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [0, "admin", ""];
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 200] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Sanyam",
     age: 25,
     hobbies: ["Music", "Movies"],
-    role: [2, "Author"],
+    role: Role.ADMIN,
 };
-person.role.push("admin");
-person.role[1] = 10;
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
@@ -17,6 +38,9 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     //   hobby.map();
+}
+if (person.role === Role.ADMIN) {
+    console.log("is Admin");
 }
 // const product: {
 //   id: string;
--- course-project/app.ts
+++ course-project/app.ts
@@ -2,21 +2,38 @@
 //   name: string;
 //   age: number;
 // };
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  name: "Sanyam",
-  age: 25,
-  hobbies: ["Music", "Movies"],
-  role: [2, "Author"],
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Sanyam",
+//   age: 25,
+//   hobbies: ["Music", "Movies"],
+//   role: [2, "Author"],
+// };
 
 // person.role.push("admin");
 // person.role[1] = 10;
-person.role = [0, "admin", ""];
+// person.role = [0, "admin", ""];
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = 100,
+  AUTHOR = 200,
+}
+
+const person = {
+  name: "Sanyam",
+  age: 25,
+  hobbies: ["Music", "Movies"],
+  role: Role.ADMIN,
+};
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
@@ -26,6 +43,10 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   //   hobby.map();
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is Admin");
 }
 
 // const product: {
