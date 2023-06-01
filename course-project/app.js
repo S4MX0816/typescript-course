45c36316864a420d55e01782067f7841b7369033
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-02 01:02:30 +0530

    function types and callbacks

--- course-project/app.js
+++ course-project/app.js
@@ -6,8 +6,13 @@ function printResult(num) {
 }
 console.log(printResult(add(5, 12)));
 // let someValue: undefined
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 var combinedValues;
 combinedValues = add;
 // combinedValues = printResult;
 // combinedValues = 5;
 console.log(combinedValues(8, 8));
+addAndHandle(10, 20, printResult);
--- course-project/app.ts
+++ course-project/app.ts
@@ -10,9 +10,18 @@ console.log(printResult(add(5, 12)));
 
 // let someValue: undefined
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 let combinedValues: (a: number, b: number) => number;
 combinedValues = add;
 // combinedValues = printResult;
 // combinedValues = 5;
 
 console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
