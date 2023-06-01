ff4d39410ebb3720677cdbb30086a843d4724edb
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-02 01:07:38 +0530

    unknown type

--- course-project/app.js
+++ course-project/app.js
@@ -1,18 +1,7 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Sam";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
-}
-console.log(printResult(add(5, 12)));
-// let someValue: undefined
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-var combinedValues;
-combinedValues = add;
-// combinedValues = printResult;
-// combinedValues = 5;
-console.log(combinedValues(8, 8));
-addAndHandle(10, 20, printResult);
--- course-project/app.ts
+++ course-project/app.ts
@@ -1,27 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Sam";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-console.log(printResult(add(5, 12)));
-
-// let someValue: undefined
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-let combinedValues: (a: number, b: number) => number;
-combinedValues = add;
-// combinedValues = printResult;
-// combinedValues = 5;
-
-console.log(combinedValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
