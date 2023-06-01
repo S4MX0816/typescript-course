05fa80ccb9ba7dcd014882c6f32095bef22105ad
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-02 00:40:24 +0530

    literal types

--- course-project/app.js
+++ course-project/app.js
@@ -1,14 +1,22 @@
-function add(input1, input2) {
+function combine(input1, input2, resultType) {
     var res;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        res = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultType === "as-number") {
+        res = +input1 + +input2;
     }
     else {
         res = input1.toString() + input2.toString();
     }
+    // if (resultType === "as-number") {
+    //   return +res;
+    // } else {
+    //   return res.toString();
+    // }
     return res;
 }
-var combinedAges = add(30, 26);
+var combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
-var combinedNames = add("Sam", "Nfs");
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Sam", "Nfs", "as-text");
 console.log(combinedNames);
--- course-project/app.ts
+++ course-project/app.ts
@@ -1,15 +1,30 @@
-function add(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultType: "as-number" | "as-text"
+) {
   let res;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    res = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultType === "as-number"
+  ) {
+    res = +input1 + +input2;
   } else {
     res = input1.toString() + input2.toString();
   }
+  // if (resultType === "as-number") {
+  //   return +res;
+  // } else {
+  //   return res.toString();
+  // }
   return res;
 }
 
-const combinedAges = add(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = add("Sam", "Nfs");
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Sam", "Nfs", "as-text");
 console.log(combinedNames);
