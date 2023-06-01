2959f846a053ac5053c4a5f319cf23eae1347b83
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-02 01:11:41 +0530

    never type

--- course-project/app.js
+++ course-project/app.js
@@ -5,3 +5,8 @@ userInput = "Sam";
 if (typeof userInput === "string") {
     userName = userInput;
 }
+function generateError(message, errorCode) {
+    throw { message: message, errorCode: errorCode };
+}
+var res = generateError("An error occurred!", 500);
+console.log(res);
--- course-project/app.ts
+++ course-project/app.ts
@@ -7,3 +7,11 @@ userInput = "Sam";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, errorCode: number): never {
+  throw { message, errorCode };
+  // while (true) {}
+}
+
+const res = generateError("An error occurred!", 500);
+console.log(res);
