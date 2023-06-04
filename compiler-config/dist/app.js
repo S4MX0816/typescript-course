78e3d2fd9752c0a7bb69eb33c2d76aeb3571b681
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-05 04:11:20 +0530

    code quality configs

--- compiler-config/dist/app.js
+++ compiler-config/dist/app.js
@@ -1,8 +1,9 @@
 "use strict";
+let appId = "abc";
 const button = document.querySelector("button");
-function clickHandler(message) {
+function clickHandler(message, age) {
     console.log("clicked! " + message);
 }
 if (button) {
-    button.addEventListener("click", clickHandler.bind(null, "You're welcome"));
+    button.addEventListener("click", clickHandler.bind(null, "You're welcome", 2));
 }
--- compiler-config/src/app.ts
+++ compiler-config/src/app.ts
@@ -1,10 +1,24 @@
+let appId = "abc";
 const button = document.querySelector("button");
 
-function clickHandler(message: string) {
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+}
+
+function clickHandler(
+  message: string
+  //  age: number
+) {
+  // let userName;
   console.log("clicked! " + message);
 }
 
 // A comment
 if (button) {
-  button.addEventListener("click", clickHandler.bind(null, "You're welcome"));
+  button.addEventListener(
+    "click",
+    clickHandler.bind(null, "You're welcome", 2)
+  );
 }
