4363e6d00937cfac1c423fbcfd37829d7f1fd07e
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-12 00:06:03 +0530

    let and const

--- next-gen-features/dist/app.js
+++ next-gen-features/dist/app.js
@@ -1,19 +1,16 @@
+"use strict";
 const userName = "Sanyam";
 // userName = "Naveen";
-
 let age = 30;
 age = 22;
-
-function add(a: number, b: number) {
-  let result;
-  result = a + b;
-  return result;
+function add(a, b) {
+    let result;
+    result = a + b;
+    return result;
 }
 // console.log(result);
-
 if (age > 20) {
-  // var isOld = true; // available outside
-  let isOld = true;
+    // var isOld = true; // available outside
+    let isOld = true;
 }
-
 // console.log(isOld); // throw error when using let
