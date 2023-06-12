ee132a34951a0c357b9cd4ad1368b9b10b21354d
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-12 21:44:51 +0530

    discriminated unions

--- advanced-types/src/app.ts
+++ advanced-types/src/app.ts
@@ -78,3 +78,33 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+//////////////////// DISCRIMINATED UNION
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird": {
+      speed = animal.flyingSpeed;
+      break;
+    }
+    case "horse": {
+      speed = animal.runningSpeed;
+    }
+  }
+  console.log("Moving with speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
