3b971218c5f8594880f9f62e8f1613fd620a0bc1
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-12 17:13:08 +0530

    access modifiers

--- classes-and-interfaces/dist/app.js
+++ classes-and-interfaces/dist/app.js
@@ -1,14 +1,26 @@
 "use strict";
 var Department = /** @class */ (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log("Department: " + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department("Accounting");
 accounting.describe();
-var accountCopy = { name: "Dummy", describe: accounting.describe };
-accountCopy.describe();
+// const accountCopy = { name: "Dummy", describe: accounting.describe };
+// accountCopy.describe();
+accounting.addEmployee("Sanyam");
+accounting.addEmployee("Naveen");
+// accounting.employees[2] = "Kushager";
+accounting.printEmployeeInformation();
--- classes-and-interfaces/src/app.ts
+++ classes-and-interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,10 +9,26 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 accounting.describe();
 
-const accountCopy = { name: "Dummy", describe: accounting.describe };
-accountCopy.describe();
+// const accountCopy = { name: "Dummy", describe: accounting.describe };
+// accountCopy.describe();
+
+accounting.addEmployee("Sanyam");
+accounting.addEmployee("Naveen");
+
+// accounting.employees[2] = "Kushager";
+
+accounting.printEmployeeInformation();
