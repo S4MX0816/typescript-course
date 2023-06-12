48ce2dcbf55f74e5b537c9206b1981d46c96e27a
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-12 17:08:10 +0530

    constructor function and this keyword

--- classes-and-interfaces/dist/app.js
+++ classes-and-interfaces/dist/app.js
@@ -3,7 +3,12 @@ var Department = /** @class */ (function () {
     function Department(n) {
         this.name = n;
     }
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.name);
+    };
     return Department;
 }());
 var accounting = new Department("Accounting");
-console.log(accounting);
+accounting.describe();
+var accountCopy = { name: "Dummy", describe: accounting.describe };
+accountCopy.describe();
