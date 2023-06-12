14ce5da608f85f92513849c2371bb1d940b13086
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-12 21:40:02 +0530

    type guards

--- advanced-types/dist/app.js
+++ advanced-types/dist/app.js
@@ -1,6 +1,48 @@
 "use strict";
+//////////////////// INTERSECTION TYPES
 const e1 = {
     name: "Sanyam",
     privileges: ["create-server"],
     startDate: new Date(),
 };
+//////////////////// TYPE GUARDS
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log(emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log(emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: "Naveen", startDate: new Date() });
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo ..." + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
