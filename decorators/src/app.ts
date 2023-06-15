2896f41d7a9e3d9cab9458316fb30615b0b5f27d
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-15 16:05:30 +0530

    returning a class in class decorator

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -8,14 +8,21 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   console.log("TEMPLATE FACTORY");
-  return function (constructor: typeof Person) {
-    console.log("Rendering template");
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      document.querySelector("h1")!.textContent = p.name;
-    }
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          document.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
