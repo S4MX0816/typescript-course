8dbe93beaae76d62eb64971defe80cecd037fc77
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-15 15:53:07 +0530

    accessor & parameter decorators

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -39,11 +39,31 @@ function Log(target: any, propertyName: string | symbol) {
   console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target, name, descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target, name, descriptor);
+}
+
+function Log4(target: any, name: string | symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target, name, position);
+}
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -57,7 +77,8 @@ class Product {
     this._price = p;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this.price * (1 + tax);
   }
 }
