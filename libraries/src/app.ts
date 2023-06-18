d1c3b051cf56eafe4361d78634ac051770269a4b
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-19 01:49:17 +0530

    using class-validators package

--- libraries/src/app.ts
+++ libraries/src/app.ts
@@ -3,6 +3,7 @@ import { plainToInstance } from "class-transformer";
 
 import _ from "lodash";
 import { Product } from "./product.model";
+import { validate } from "class-validator";
 
 declare var GLOBAL: any;
 
@@ -26,3 +27,13 @@ const loadedProducts = plainToInstance(Product, products);
 for (const prod of loadedProducts) {
   console.log(prod.getInformation());
 }
+
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERRORS!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
