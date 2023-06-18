ec842665deaa2ca8f5d571024925abeb45d4f60c
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-19 01:43:10 +0530

    using class-transformer package

--- libraries/src/app.ts
+++ libraries/src/app.ts
@@ -1,7 +1,28 @@
+import "reflect-metadata";
+import { plainToInstance } from "class-transformer";
+
 import _ from "lodash";
+import { Product } from "./product.model";
 
 declare var GLOBAL: any;
 
 console.log(_.shuffle([1, 2, 3]));
 
 console.log(GLOBAL);
+
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
+
+// const p1 = new Product("A Book", 12.99);
+
+// const loadedProducts = products.map(
+//   (prod) => new Product(prod.title, prod.price)
+// );
+
+const loadedProducts = plainToInstance(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
