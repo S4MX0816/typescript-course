53f3c889cec53159a5de25d60d5aa17194a6b013
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-20 20:13:03 +0530

    fetching location coordinates

--- select-&-share-a-place/src/app.ts
+++ select-&-share-a-place/src/app.ts
@@ -1,11 +1,52 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address") as HTMLInputElement;
+
+type NominatimGeoJSON = {
+  type: string;
+  licence: string;
+  features: FeatureGeoJSON[];
+};
+
+type FeatureGeoJSON = {
+  type: string;
+  properties: PropertiesFeatures;
+  bbox: [number, number, number, number];
+  geometry: GeometryFeatures;
+};
+
+type PropertiesFeatures = {
+  place_id: string;
+  osm_type: string;
+  osm_id: string;
+  display_name: string;
+  place_rank: string;
+  category: string;
+  type: string;
+  importance: number;
+};
+
+type GeometryFeatures = {
+  type: string;
+  coordinates: [number, number];
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
-  // send this to Google's API!
+  axios
+    .get<NominatimGeoJSON>(
+      `https://nominatim.openstreetmap.org/search/${encodeURI(
+        enteredAddress
+      )}?format=geojson`
+    )
+    .then((res) => {
+      const coordinates = res.data.features[0].geometry.coordinates;
+      console.log(coordinates);
+    })
+    .catch((err) => console.log(err));
 }
 
 form.addEventListener("submit", searchAddressHandler);
