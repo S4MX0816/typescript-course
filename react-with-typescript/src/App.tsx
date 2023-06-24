508a1512c7bbd02390482f20a7764b0af054815b
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-24 18:45:10 +0530

    managing state better

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -7,7 +7,10 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos([{ id: Math.random.toString(), text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text },
+    ]);
     console.log(text);
   };
 
