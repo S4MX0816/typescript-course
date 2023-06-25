a530c55415f1025ecf8b1cf6a4aebbc5f575fb4e
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-25 19:30:21 +0530

    deleting a todo

--- express-with-typescript/src/controllers/todos.ts
+++ express-with-typescript/src/controllers/todos.ts
@@ -33,3 +33,16 @@ export const updateTodo: RequestHandler<
   TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
   res.status(201).json({ message: "Updated!", todo: TODOS[todoIndex] });
 };
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS.splice(todoIndex, 1);
+  res.status(201).json({ message: "Todo deleted!" });
+};
