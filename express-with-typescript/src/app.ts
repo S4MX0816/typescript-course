43519ff2f8597f6890100c69c12fd8fbb22b8306
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-25 18:58:43 +0530

    working with controllers, models and parsing request bodies

--- express-with-typescript/src/app.ts
+++ express-with-typescript/src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
+import { json } from "body-parser";
 
 import todosRouter from "./routes/todos";
 
 const app = express();
+
+app.use(json());
 
 app.use("/todos", todosRouter);
 
@@ -10,4 +13,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 });
 
-app.listen(300);
+app.listen(3000);
