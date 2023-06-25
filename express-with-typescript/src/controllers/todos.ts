275aca252bce7f4483b92ed64375884e04f1e43e
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-06-25 19:25:45 +0530

    update a todo

--- express-with-typescript/src/controllers/todos.ts
+++ express-with-typescript/src/controllers/todos.ts
@@ -11,3 +11,25 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
   res.status(201).json({ message: "Created the todo.", createdTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.status(201).json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<
+  { id: string },
+  any,
+  { text: string }
+> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updatedText = req.body.text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+  res.status(201).json({ message: "Updated!", todo: TODOS[todoIndex] });
+};
